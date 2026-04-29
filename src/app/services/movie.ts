2b18cb738f0039062130507a149f898bb099edb8
unknown <user@nb-allo-m124.nautilus.allo.ua>
2026-04-29 23:36:00 +0100

    Added TMDB API service

--- src/app/services/movie.ts
+++ src/app/services/movie.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
-export class Movie {
-  
+export class MovieService {
+
+  private apiKey = '391592306b541274f588fc0d6c0ab983';
+  private baseUrl = 'https://api.themoviedb.org/3';
+
+  constructor(private http: HttpClient) {}
+
+  getTrendingMovies() {
+    return this.http.get(`${this.baseUrl}/trending/movie/day?api_key=${this.apiKey}`);
+  }
 }
