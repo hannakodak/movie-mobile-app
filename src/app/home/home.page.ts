9286ed37c3904cb58cafcf1a4907e41d25168e64
hannakodak <an.v.kodak@gmail.com>
2026-05-03 00:08:22 +0100

    Implement home page. Display trending movies with overview and poster + fixes

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import { MovieService } from '../services/movie';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, CommonModule],
 })
 export class HomePage {
 
@@ -20,7 +21,7 @@ export class HomePage {
     this.movieService.getTrendingMovies().subscribe(data => {
       
       //save movies to show on screen
-      this.movies = (data as any).result;
+      this.movies = (data as any).results;
     });
   }
 }
