a6d6ecd9386d0b1659169a5919bf51501c2cb428
hannakodak <an.v.kodak@gmail.com>
2026-05-03 22:40:17 +0100

    Add home navigation button to favourites page

--- src/app/favourites/favourites.page.ts
+++ src/app/favourites/favourites.page.ts
@@ -1,20 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar,  IonButton} from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar,  IonButton, IonIcon, IonButtons} from '@ionic/angular/standalone';
 import { FavouritesService } from '../services/favourites';
+import { ActivatedRoute, Router } from '@angular/router';
+import {addIcons} from 'ionicons';
+import {home} from 'ionicons/icons';
 
 @Component({
   selector: 'app-favourites',
   templateUrl: './favourites.page.html',
   styleUrls: ['./favourites.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule,  IonButton]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule,  IonButton, IonIcon, IonButtons]
 })
 export class FavouritesPage implements OnInit {
   favourites: any[] = [];
 
-  constructor(private favouritesService: FavouritesService) { }
+  constructor(
+    private favouritesService: FavouritesService,
+    private route: ActivatedRoute,
+    private router: Router) { 
+    addIcons({home});
+  }
 
   ngOnInit() {
     // get saved favourites from localstorage
@@ -28,4 +36,10 @@ remove(movieId: number) {
   //refresh list after removal
   this.favourites = this.favouritesService.getFavourites();
 }
+
+  // go back home page 
+  goHome(){
+    this.router.navigate(['home'])
+  }
+  
 }
